89ecd61917af6941a026e10cf6c957ebb2d910a1
zhuoIC <nicolimonar@gmail.com>
2017-04-20 13:36:54 +0200

    Añadidos archivos

---
+++ Ejercicio_03/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_03
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int num;
+        static Random rnd = new Random();
+        static int intentos = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            lblNum.Opacity = 100;
+        }
+
+        private void cbxVerNumero_Unchecked(object sender, RoutedEventArgs e)
+        {
+            lblNum.Opacity = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            num = rnd.Next(1,101);
+            lblNum.Content = num;
+            btnGenerar.IsEnabled = false;
+            btnProbar.IsEnabled = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int candidato = int.Parse(tbxAcertar.Text);
+                intentos++;
+                if (candidato == num)
+                {
+                    lblResultado.Content = "";
+                    MessageBox.Show("¡Has acertado el número!", "¡ENHORABUENA!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                    btnGenerar.IsEnabled = true;
+                    btnProbar.IsEnabled = false;
+                }
+                else if (candidato < num)
+                {
+                    lblResultado.Content = "NO, el número buscado es MAYOR";
+                    tbxAcertar.Focus();
+                }
+                else
+                {
+                    lblResultado.Content = "NO, el número buscado es MENOR";
+                    tbxAcertar.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Vuelve a intentarlo");
+            }
+        }
+    }
+}
---
+++ Ejercicio_9/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//---------------------------------------------
+
+namespace Ejercicio_9
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int[] dado = new int[6];
+        static Random rnd = new Random();
+        int nTiradas = 0;
+        static string[] imagenes = new string[] 
+        { 
+            @"..\..\Imagenes\1.png",
+            @"..\..\Imagenes\2.png",
+            @"..\..\Imagenes\3.png",
+            @"..\..\Imagenes\4.png",
+            @"..\..\Imagenes\5.png",
+            @"..\..\Imagenes\6.png"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTirar_Click(object sender, RoutedEventArgs e)
+        {
+            TirarDado(1);
+        }
+
+        private void TirarDado(int nVeces)
+        {
+            int contador = 0;
+            int resultado = 0;
+            while (nVeces > contador)
+            {
+                resultado = rnd.Next(dado.Length);
+                dado[resultado]++;
+                MostrarResuLtado(resultado);
+                MostrarImagen(resultado);
+                contador++;
+            }
+            MostrarEstadistica();
+        }
+
+        private void MostrarImagen(int resultado)
+        {
+            BitmapImage img = new BitmapImage(new Uri(System.IO.Path.GetFullPath(imagenes[resultado])));
+            imgDado.Source = img;
+        }
+
+        private void MostrarResuLtado(int resultado)
+        {
+            tbxResultado.Text += ++nTiradas + "→" + (resultado+1)+"\r\n";
+        }
+
+        private void MostrarEstadistica()
+        {
+            int contador = 1;
+            tbxEstadistica.Text = "";
+            foreach (int cara in dado)
+            {
+                tbxEstadistica.Text += contador + "→" + cara + "→"+(cara*100)/nTiradas+"%\r\n";
+                contador++;
+            }
+        }
+
+        private void btnAuto_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbxNTiradas.Text.Length > 0)
+            {
+                int nTiradas = int.Parse(tbxNTiradas.Text);
+                TirarDado(nTiradas);
+            }
+        }
+
+    }
+}
