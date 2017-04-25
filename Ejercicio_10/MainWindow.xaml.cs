41e9509575e18e47900dd69072c3c233266c661b
Nicolás Hernández Jiménez <nicolimonar@gmail.com>
2017-04-25 21:36:12 +0200

    Cambios

--- Ejercicio_10/MainWindow.xaml.cs
+++ Ejercicio_10/MainWindow.xaml.cs
@@ -20,18 +20,54 @@ namespace Ejercicio_10
     /// </summary>
     public partial class MainWindow : Window
     {
+        StringBuilder contenido = new StringBuilder();
+        bool esNumero = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        
+        private void Numero_Click(object sender, RoutedEventArgs e)
+        {
+            ActualizarTextbox(sender);
+        }
 
-        
+        private void ActualizarTextbox(object sender)
+        {
 
-        
+            contenido.Append(((Button)sender).Content.ToString());
+            tbxCalculo.Text = contenido.ToString();
+        }
 
-        
+        private void Operador_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCero_Click(object sender, RoutedEventArgs e)
+        {
+            int num = -1;
+            if (contenido.Length > 0)
+            {
+                esNumero = int.TryParse(contenido[contenido.Length - 1].ToString(), out num);
+            }
+            if (num > 0 || esNumero)
+            {
+                ActualizarTextbox(sender);
+                if (!esNumero)
+                {
+                    esNumero = true;
+                }
+            }
+
+
+        }
 
     }
 }
