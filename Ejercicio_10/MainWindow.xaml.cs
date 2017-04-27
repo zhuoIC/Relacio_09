ab26eea316a2a7b9f98c1003d9d5a30713b8e1ea
zhuoIC <nicolimonar@gmail.com>
2017-04-27 14:32:56 +0200

    Calculadora en fase alpha. Ya hace algunos cálculos.

--- Ejercicio_10/MainWindow.xaml.cs
+++ Ejercicio_10/MainWindow.xaml.cs
@@ -20,16 +20,101 @@ namespace Ejercicio_10
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] operandos = new string[3];
+        int nDatos = 0;
+        string resultado = string.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Numero_Click(object sender, RoutedEventArgs e)
+        {
+            if (nDatos == 0 || nDatos == 2)
+            {
+                nDatos++;
+            }
+            operandos[nDatos - 1] = ((Button)sender).Content.ToString();
+
+            tbxCalculo.Text = ((Button)sender).Content.ToString();
+            btnSignoUnitario.IsEnabled = true;
+        }
+
+        private void Operador_Click(object sender, RoutedEventArgs e)
+        {
+            if (nDatos > 0)
+            {
+                if (btnSignoUnitario.IsEnabled)
+                {
+                    btnSignoUnitario.IsEnabled = false;
+                }
+                switch (nDatos)
+                {
+                    case 1:
+                        operandos[nDatos++] = ((Button)sender).Content.ToString();
+                        break;
+                    case 2:
+                        operandos[nDatos - 1] = ((Button)sender).Content.ToString();
+                        break;
+                }
+
+            }
+        }
+
+        void Calcular()
+        {
+            if (nDatos == 3)
+            {
+                switch (operandos[1])
+                {
+                    case "*":
+                        operandos[0] = (double.Parse(operandos[0]) * double.Parse(operandos[2])).ToString();
+                        break;
+                    case "+":
+                        operandos[0] = (double.Parse(operandos[0]) + double.Parse(operandos[2])).ToString();
+                        break;
+                    case "-":
+                        operandos[0] = (double.Parse(operandos[0]) - double.Parse(operandos[2])).ToString();
+                        break;
+                    case "/":
+                        operandos[0] = (double.Parse(operandos[0]) / double.Parse(operandos[2])).ToString();
+                        break;
+                }
+                nDatos = 0;
+                tbxCalculo.Text = operandos[nDatos++];
+            }
+        }
+
+        private void btlLimpiarTodo_Click(object sender, RoutedEventArgs e)
+        {
+            nDatos = 0;
+            resultado = string.Empty;
+            tbxCalculo.Text = "0";
+        }
+
+        private void btnCero_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        
 
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            Calcular();
+        }
+
+        private void btnSignoUnitario_Click(object sender, RoutedEventArgs e)
+        {
+            switch (nDatos)
+            {
+                case 0:
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+            }
+        }
     }
 }
