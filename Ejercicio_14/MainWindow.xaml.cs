e80ec6c576ae9806ef6f7e48331a847c176e755b
Nicolás Hernández Jiménez <nicolimonar@gmail.com>
2017-05-18 01:04:00 +0200

    Conexion establecida con ficheros Ejercicio 14

--- Ejercicio_14/MainWindow.xaml.cs
+++ Ejercicio_14/MainWindow.xaml.cs
@@ -31,6 +31,40 @@ namespace Ejercicio_14
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog abrirDoc = new OpenFileDialog();
+            abrirDoc.Multiselect = false;
+            
+            abrirDoc.ShowDialog();
+            if (abrirDoc.CheckFileExists)
+            {
+                int nLineas = 0;
+                int nPalabras = 0;
+                FileInfo archivo = new FileInfo(abrirDoc.FileName);
+                long tamanio = archivo.Length/1024;
+                using (StreamReader flujo = new StreamReader(archivo.OpenRead()))
+                {
+                    string linea = string.Empty;
+                    
+                    while ((linea = flujo.ReadLine()) != null)
+                    {
+                        bool EsLetraoDig = false;
+                        for (int i = 0; i < linea.Length; i++)
+                        {
+                            if (char.IsLetterOrDigit(linea[i]))
+                            {
+                                if (!EsLetraoDig)
+                                {
+                                    EsLetraoDig = true;
+                                    nPalabras++;
+                                }  
+                            }
+                            else
+                            {
+                                EsLetraoDig = false;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
