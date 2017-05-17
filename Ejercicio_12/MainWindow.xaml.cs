c1c404e17a1d50a9797d06afbf34bc80d2ef3f1b
zhuoIC <nicolimonar@gmail.com>
2017-05-17 12:50:21 +0200

    Terminado ejercicio 12

---
+++ Ejercicio_12/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Ejercicio_12
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            const byte ALPHA = 255;
+            byte colorRojo = (byte)(sldRojo.Value);
+            byte colorVerde = (byte)(sldVerde.Value);
+            byte colorAzul = (byte)(sldAzul.Value);
+            stcPanel.Background = new SolidColorBrush(Color.FromArgb(ALPHA, colorRojo, colorVerde, colorAzul));
+        }
+    }
+}
