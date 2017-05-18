7801d41ee07ebaff69f1f1b32a81df016f73bab4
zhuoIC <nicolimonar@gmail.com>
2017-05-18 09:35:05 +0200

    Interfaz completada. Ejercicio 14 terminado.

--- Ejercicio_14/MainWindow.xaml.cs
+++ Ejercicio_14/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 //----------------------------------
 using System.IO;
 using Microsoft.Win32;
@@ -28,42 +16,84 @@ namespace Ejercicio_14
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnExaminar_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog abrirDoc = new OpenFileDialog();
             abrirDoc.Multiselect = false;
-            
+
+            abrirDoc.FileOk += abrirDoc_FileOk;
             abrirDoc.ShowDialog();
-            if (abrirDoc.CheckFileExists)
+        }
+
+        void abrirDoc_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            OpenFileDialog doc = ((OpenFileDialog)sender);
+            if (doc.CheckFileExists)
             {
-                int nLineas = 0;
-                int nPalabras = 0;
-                FileInfo archivo = new FileInfo(abrirDoc.FileName);
-                long tamanio = archivo.Length/1024;
-                using (StreamReader flujo = new StreamReader(archivo.OpenRead()))
+                string ruta = doc.FileName;
+                tbxRuta.Text = ruta;
+                AccederArchivo(ruta);
+            }
+        }
+
+        private void AccederArchivo(string ruta)
+        {
+            int nLineas = 0;
+            int nPalabras = 0;
+            FileInfo archivo = new FileInfo(ruta);
+            string tamanio = ((double)archivo.Length / 1024).ToString("0.000") + " kB";
+            using (StreamReader flujo = new StreamReader(archivo.OpenRead()))
+            {
+                string linea = string.Empty;
+
+                while ((linea = flujo.ReadLine()) != null)
                 {
-                    string linea = string.Empty;
-                    
-                    while ((linea = flujo.ReadLine()) != null)
+                    nLineas++;
+                    bool EsLetraoDig = false;
+                    for (int i = 0; i < linea.Length; i++)
                     {
-                        bool EsLetraoDig = false;
-                        for (int i = 0; i < linea.Length; i++)
+                        if (char.IsLetterOrDigit(linea[i]))
                         {
-                            if (char.IsLetterOrDigit(linea[i]))
+                            if (!EsLetraoDig)
                             {
-                                if (!EsLetraoDig)
-                                {
-                                    EsLetraoDig = true;
-                                    nPalabras++;
-                                }  
+                                EsLetraoDig = true;
+                                nPalabras++;
                             }
-                            else
-                            {
-                                EsLetraoDig = false;
-                            }
+                        }
+                        else
+                        {
+                            EsLetraoDig = false;
                         }
                     }
                 }
+            }
+
+            MostrarDatos(nLineas, nPalabras, archivo, tamanio);
+        }
+
+        private void MostrarDatos(int nLineas, int nPalabras, FileInfo archivo, string tamanio)
+        {
+            lblAtributos.Content = archivo.Attributes;
+            lblLineas.Content = nLineas;
+            lblPalabras.Content = nPalabras;
+            lblTamanio.Content = tamanio;
+        }
+
+        private void btnAbrir_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string ruta = Path.GetFullPath(tbxRuta.Text);
+
+                if (File.Exists(ruta))
+                {
+                    AccederArchivo(ruta);
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("No se pudo encontrar el archivo", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
     }
